e5a6317264f525a4825623d7a6fe65ab13d0f6b4
Teodor Ande Elstad <teodoran@stud.ntnu.no>
2014-01-21 15:34:07 +0100

    Backup commit.

--- custom-binding-handlers/area-chart-binding.js
+++ custom-binding-handlers/area-chart-binding.js
@@ -5,71 +5,55 @@ ko.bindingHandlers.areaChart = {
         "use strict";
         var margin = {top: 20, right: 20, bottom: 30, left: 50},
 
-        elementWidth = parseInt(d3.select(element).style("width"), 10),
-        elementHeight = parseInt(d3.select(element).style("height"), 10),
+            elementWidth = parseInt(d3.select(element).style("width"), 10),
+            elementHeight = parseInt(d3.select(element).style("height"), 10),
 
-        width = elementWidth - margin.left - margin.right,
-        height = elementHeight - margin.top - margin.bottom;
+            width = elementWidth - margin.left - margin.right,
+            height = elementHeight - margin.top - margin.bottom;
 
-        var parseDate = d3.time.format("%d-%b-%y").parse;
-
-        var x = d3.time.scale()
-        .range([0, width]);
+        var x = d3.scale.linear()
+            .range([0, width]);
 
         var y = d3.scale.linear()
-        .range([height, 0]);
+            .range([height, 0]);
 
         var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+            .scale(x)
+            .orient("bottom");
 
         var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+            .scale(y)
+            .orient("left");
 
         var area = d3.svg.area()
-        .x(function(d) { return x(d.date); })
-        .y0(height)
-        .y1(function(d) { return y(d.close); });
+            .x(function(d) { return x(d.x); })
+            .y0(height)
+            .y1(function(d) { return y(d.y); });
 
         var svg = d3.select(element).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var data = [
-        {date: parseDate("1-May-12"), close: 282.13},
-        {date: parseDate("30-Apr-12"), close: 383.98},
-        {date: parseDate("27-Apr-12"), close: 303.00},
-        {date: parseDate("26-Apr-12"), close: 207.70},
-        {date: parseDate("25-Apr-12"), close: 410.00},
-        {date: parseDate("24-Apr-12"), close: 460.28},
-        {date: parseDate("23-Apr-12"), close: 371.70},
-        {date: parseDate("20-Apr-12"), close: 472.98},
-        {date: parseDate("19-Apr-12"), close: 587.44},
-        {date: parseDate("18-Apr-12"), close: 408.34},
-        {date: parseDate("17-Apr-12"), close: 309.70},
-        {date: parseDate("16-Apr-12"), close: 280.13}
-        ];
+        var data = ko.unwrap(valueAccessor());
 
-
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain([0, d3.max(data, function(d) { return d.close; })]);
+        x.domain(d3.extent(data, function(d) { return d.x; }));
+        y.domain([0, d3.max(data, function(d) { return d.y; })]);
 
         svg.append("path")
-        .datum(data)
-        .attr("class", "area")
-        .attr("d", area);
+            .datum(data)
+            .attr("class", "area")
+            .attr("d", area);
 
         svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
 
         svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
+            .attr("class", "y axis")
+            .call(yAxis)
     },
     update: function(element, valueAccessor) {
         "use strict";
--- data-view-model.js
+++ data-view-model.js
@@ -18,24 +18,37 @@ var D3KD = this.D3KD || {};
                 ];
             },
 
-            randomBros = function () {
-                return [
-                    { name: "Kiro", value: Math.random() },
-                    { name: "Steffen", value: Math.random() },
-                    { name: "Teodor", value: Math.random() },
-                    { name: "Peter", value: Math.random() }
-                ];
-            },
-
             updateBarChartData = function() {
                 self.barChartData(randomPonies());
+            },
+
+            updateAreaChartData = function () {
+                console.log(self.areaChartData()[0]);
             };
 
 
         self.barChartData = ko.observable(randomPonies());
-
         self.barChartSmall1 = ko.observable(randomPonies());
         self.barChartSmall2 = ko.observable(randomPonies());
+
+        self.areaChartData = ko.observableArray([
+            {x: 1, y: 100},
+            {x: 2, y: 130},
+            {x: 3, y: 140},
+            {x: 4, y: 150},
+            {x: 5, y: 135},
+            {x: 6, y: 125},
+            {x: 7, y: 110},
+            {x: 8, y: 100},
+            {x: 9, y: 80},
+            {x: 10, y: 70},
+            {x: 11, y: 50},
+            {x: 12, y: 20}
+        ]);
+
+        updateAreaChartData();
+
+        console.log(self.areaChartData()[self.areaChartData().length - 1]);
 
         setInterval(updateBarChartData, 2000);
     };
