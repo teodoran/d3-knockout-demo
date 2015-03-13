75462c994d33b8dd4d11f9f80988d2b83589d0db
Magnus Kirø <magnuskiro@gmail.com>
2015-03-13 16:19:20 +0100

    fixed bug, and updated the first example.

--- view-models/election-data-view-model.js
+++ view-models/election-data-view-model.js
@@ -5,15 +5,22 @@ var D3KD = this.D3KD || {};
 (function (namespace) {
     "use strict";
     namespace.dataViewModel = function() {
-        var self = this;
+        var self = this,
+
+            getNum = function () {
+                return Math.random() * 100
+                //return 1;
+            };
 
         self.electionData = ko.observable(37);
 
-        self.updateElectionData = function () {
-            return self.electionData(Math.random() * 100);
-        };
-
-        setInterval(updateElectionData(), 1000);
+        setInterval(function () {
+            self.electionData(
+                // method call here to change the chart data.
+                getNum()
+                //Math.random() * 100
+            );
+        }, 1000);
 
     };
 }(D3KD));
