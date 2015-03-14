ff2a47137a28b1808d01c7245059618dea254582
Magnus Kirø <magnuskiro@gmail.com>
2015-03-14 19:47:35 +0100

    bug fix, and removeAll() test/example.

--- view-models/line-data-view-model.js
+++ view-models/line-data-view-model.js
@@ -7,10 +7,32 @@ var D3KD = this.D3KD || {};
     namespace.dataViewModel = function() {
         var self = this,
 
+            stockChange = function (previousValue) {
+                var volatility = 0.7,
+                    bias = 1.5,
+                    change = bias * volatility * Math.random();
+
+                if (change > volatility || previousValue - change <= 0.1) {
+                    return previousValue + change;
+                }
+
+                return previousValue - change;
+            },
+
+            lineDataPoint = function (previousValue) {
+                return {
+                    date: new Date(),
+                    close: stockChange(previousValue || Math.random() * 10)
+                };
+            },
+
             updateLineChartData = function () {
-                var previousValue = self.lineChartData()[
-                    self.lineChartData().length - 1
-                ].close;
+                // if there are elements in the array, set previous value.
+                if (self.lineChartData.size>0){
+                    var previousValue = self.lineChartData()[
+                        self.lineChartData().length - 1
+                        ].close;
+                }
                 return self.lineChartData.push(lineDataPoint(previousValue));
             };
 
@@ -19,6 +41,16 @@ var D3KD = this.D3KD || {};
         ]);
 
         setInterval(updateLineChartData, 1000);
+
+        // interval for removing all data in array/lineChartData.
+        setInterval(function(){
+                //console.log("remove data");
+                self.lineChartData.removeAll();
+            }
+            // every 10 sec.
+            , 10000);
+
+        //console.log("initiated");
 
     };
 }(D3KD));
